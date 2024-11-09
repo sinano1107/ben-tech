103759768e45f48e1bb48d9104e231fe1d59ad76
sinano1107 <tftf161107@icloud.com>
2024-11-09 21:33:29 +0900

    update サーボモーター

---
+++ edge/SERVO_MOTOR/main.py
@@ -0,0 +1,19 @@
+from machine import Pin, PWM
+import utime
+
+servo = PWM(Pin(0))
+servo.freq(50)
+
+
+# 角度(degree)からデュティー比を0〜65535 の範囲の値として返す関数
+def servo_value(degree):
+    return int((degree * 9.5 / 180 + 2.5) * 65535 / 100)
+
+
+while True:
+    servo.duty_u16(servo_value(0))
+    utime.sleep(1)
+    servo.duty_u16(servo_value(90))
+    utime.sleep(1)
+    servo.duty_u16(servo_value(180))
+    utime.sleep(1)
