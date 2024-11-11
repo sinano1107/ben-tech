6df332f5636486b72f8fb43f01f907098356f990
sinano1107 <tftf161107@icloud.com>
2024-11-11 18:29:02 +0900

    update AutoFlushManagerを追加

--- edge/BT-hub.py
+++ edge/BT-hub.py
@@ -2,138 +2,26 @@
 
 import asyncio
 import aioble
-import bluetooth
 from machine import Pin
 from micropython import const
+from common_hub import ResponsiveDeviceManager, ControllableDeviceManager
+
 
 MOCKVAR_is_detection_started = False
 
 led = Pin("LED", Pin.OUT)
 
-auto_flusher = None
 deodorant = None
-
-
-class BenTechDeviceManager:
-    def __init__(self, name):
-        self.name = const(name)
-        self.device = None
-        self.connection = None
-        self.services = {}
-        self.characteristics = {}
-
-    def is_having_device(self):
-        return self.device is not None
-
-    def is_this_device_your_charge(self, result):
-        if self.device is not None:
-            return
-        if result.name() == self.name:
-            self.device = result.device
-
-    async def connect(self):
-        if self.device is None:
-            self._log("[connect] deviceを保持していません")
-            return
-        self.connection = await self.device.connect()
-        self._log("接続完了")
-
-    async def disconnect(self):
-        if self.connection is None:
-            self._log("[disconnect] 接続されていません")
-            return
-        await self.connection.disconnect()
-        self._log("接続解除完了")
-
-    async def disconnect(self):
-        if self.connection is None:
-            self._log("コネクションを保持していないので接続解除の必要がありません")
-            return
-        await self.connection.disconnect()
-        self._log("接続を解除しました")
-
-    async def get_service(self, id):
-        if self.connection is None:
-            self._log("接続されていません")
-            return
-        if self.services.get(id) is None:
-            self.services[id] = await self.connection.service(bluetooth.UUID(id))
-        return self.services[id]
-
-    async def get_characteristic(self, service_id, char_id):
-        if self.connection is None:
-            self._log("接続されていません")
-            return
-        service_is_nothing = self.characteristics.get(service_id) is None
-        char_is_nothing = (
-            service_is_nothing
-            or self.characteristics.get(service_id).get(char_id) is None
-        )
-        if char_is_nothing:
-            service = await self.get_service(service_id)
-            char = await service.characteristic(bluetooth.UUID(char_id))
-            if service_is_nothing:
-                self.characteristics[service_id] = {}
-            self.characteristics[service_id][char_id] = char
-        return self.characteristics[service_id][char_id]
-
-    def _log(self, msg):
-        print(f"[{self.name}] {msg}")
-
-
-class ControllableDeviceManager(BenTechDeviceManager):
-    def __init__(self, name, control_service_id, control_char_id):
-        super().__init__(name)
-        self.control_service_id = control_service_id
-        self.control_char_id = control_char_id
-
-    async def control(self, value):
-        char = await self.get_characteristic(
-            self.control_service_id, self.control_char_id
-        )
-        if char is None:
-            self._log("characteristicが不明のためコントロールできません")
-            return
-        await char.write(value)
-
-
-class ResponsiveDeviceManager(ControllableDeviceManager):
-    def __init__(
-        self,
-        name,
-        control_service_id,
-        control_char_id,
-        response_service_id,
-        response_char_id,
-    ):
-        super().__init__(name, control_service_id, control_char_id)
-        self.response_service_id = response_service_id
-        self.response_char_id = response_char_id
-
-    async def listen_response(
-        self,
-        callback=lambda data: print(
-            f"レスポンスを受け取りましたがコールバックが設定されていません\n\tdata:{data}"
-        ),
-    ):
-        char = await self.get_characteristic(
-            self.response_service_id, self.response_char_id
-        )
-        if char is None:
-            self._log("charactaristicが不明のためresponseを待てません")
-            return
-        data = await char.notified()
-        callback(data)
 
 
 class LidControllerManager(ResponsiveDeviceManager):
     def __init__(self):
-        service_id = const("ac6dd643-a32e-42fb-836d-8130790d9ab4")
+        self.service_id = const("ac6dd643-a32e-42fb-836d-8130790d9ab4")
         super().__init__(
             name=const("BT-lid-controller"),
-            control_service_id=service_id,
+            control_service_id=self.service_id,
             control_char_id=const("74779bc7-1e28-4cb1-8dd7-3a3f2a9259ab"),
-            response_service_id=service_id,
+            response_service_id=self.service_id,
             response_char_id=const("82bdb1a9-4ffd-4a97-8b5f-af7e84655133"),
         )
 
@@ -164,9 +52,9 @@ class PaperObserverManager(ResponsiveDeviceManager):
         self.service_id = const("33d5f2a5-3c6e-4fc0-8f2f-05a76938a929")
         super().__init__(
             name="BT-paper-manager",
-            control_service_id=service_id,
+            control_service_id=self.service_id,
             control_char_id=const("e20af759-61d6-4406-819a-de6748d4e243"),
-            response_service_id=service_id,
+            response_service_id=self.service_id,
             response_char_id=const("5ff44fbd-11ef-46db-b3d5-794ee0f88449"),
         )
 
@@ -185,24 +73,35 @@ class PaperObserverManager(ResponsiveDeviceManager):
         await super().listen_response()
 
 
+class AutoFlusherManager(ControllableDeviceManager):
+    def __init__(self):
+        super().__init__(
+            name=const("BT-auto-flusher"),
+            control_service_id=const("6408f4f4-5002-4787-8c6f-c44147b06802"),
+            control_char_id=const("f36a79b8-f196-4975-8e53-15ed99efa275"),
+        )
+
+    async def flush(self):
+        await self.control(b"\x01")
+        self._log("水を流すように指示しました")
+
+
 lid_controller_manager = LidControllerManager()
 paper_observer_manager = PaperObserverManager()
+auto_flusher_manager = AutoFlusherManager()
 
-auto_flusher_name = "BT-auto-flusher"
 deodorant_name = "BT-deodorant"
 
-auto_flusher_connection = None
 deodorant_connection = None
 
 
 async def scan():
-    global auto_flusher, auto_flusher_name
     global deodorant, deodorant_name
 
     def calk_should_break():
         return (
             lid_controller_manager.is_having_device()
-            and auto_flusher is not None
+            and auto_flusher_manager.is_having_device()
             and deodorant is not None
             and paper_observer_manager.is_having_device()
         )
@@ -215,9 +114,8 @@ async def scan():
             # 各種デバイスを発見したら変数に代入
             lid_controller_manager.is_this_device_your_charge(result)
             paper_observer_manager.is_this_device_your_charge(result)
-            if auto_flusher is None and result.name() == auto_flusher_name:
-                auto_flusher = result.device
-            elif deodorant is None and result.name() == deodorant_name:
+            auto_flusher_manager.is_this_device_your_charge(result)
+            if deodorant is None and result.name() == deodorant_name:
                 deodorant = result.device
 
             if calk_should_break():
@@ -228,14 +126,13 @@ async def scan():
 
 
 async def connect():
-    global auto_flusher, auto_flusher_connection
     global deodorant, deodorant_connection
 
     await asyncio.gather(
-        lid_controller_manager.connect(), paper_observer_manager.connect()
+        lid_controller_manager.connect(),
+        paper_observer_manager.connect(),
+        auto_flusher_manager.connect(),
     )
-    if auto_flusher is not None:
-        auto_flusher_connection = await auto_flusher.connect()
     if deodorant is not None:
         deodorant_connection = await deodorant.connect()
 
@@ -299,6 +196,9 @@ async def main():
                 # ペーパー測定機へ測定を終了するように指示
                 paper_observer_manager.stop_observe(),
             )
+
+            # 蓋を閉じたら水を流す
+            await auto_flusher_manager.flush()
 
         await asyncio.sleep(0.1)
 
---
+++ edge/common_hub.py
@@ -0,0 +1,114 @@
+from micropython import const
+import bluetooth
+
+
+class BenTechDeviceManager:
+    def __init__(self, name):
+        self.name = const(name)
+        self.device = None
+        self.connection = None
+        self.services = {}
+        self.characteristics = {}
+
+    def is_having_device(self):
+        return self.device is not None
+
+    def is_this_device_your_charge(self, result):
+        if self.device is not None:
+            return
+        if result.name() == self.name:
+            self.device = result.device
+
+    async def connect(self):
+        if self.device is None:
+            self._log("[connect] deviceを保持していません")
+            return
+        self.connection = await self.device.connect()
+        self._log("接続完了")
+
+    async def disconnect(self):
+        if self.connection is None:
+            self._log("[disconnect] 接続されていません")
+            return
+        await self.connection.disconnect()
+        self._log("接続解除完了")
+
+    async def disconnect(self):
+        if self.connection is None:
+            self._log("コネクションを保持していないので接続解除の必要がありません")
+            return
+        await self.connection.disconnect()
+        self._log("接続を解除しました")
+
+    async def get_service(self, id):
+        if self.connection is None:
+            self._log("接続されていません")
+            return
+        if self.services.get(id) is None:
+            self.services[id] = await self.connection.service(bluetooth.UUID(id))
+        return self.services[id]
+
+    async def get_characteristic(self, service_id, char_id):
+        if self.connection is None:
+            self._log("接続されていません")
+            return
+        service_is_nothing = self.characteristics.get(service_id) is None
+        char_is_nothing = (
+            service_is_nothing
+            or self.characteristics.get(service_id).get(char_id) is None
+        )
+        if char_is_nothing:
+            service = await self.get_service(service_id)
+            char = await service.characteristic(bluetooth.UUID(char_id))
+            if service_is_nothing:
+                self.characteristics[service_id] = {}
+            self.characteristics[service_id][char_id] = char
+        return self.characteristics[service_id][char_id]
+
+    def _log(self, msg):
+        print(f"[{self.name}] {msg}")
+
+
+class ControllableDeviceManager(BenTechDeviceManager):
+    def __init__(self, name, control_service_id, control_char_id):
+        super().__init__(name)
+        self.control_service_id = control_service_id
+        self.control_char_id = control_char_id
+
+    async def control(self, value):
+        char = await self.get_characteristic(
+            self.control_service_id, self.control_char_id
+        )
+        if char is None:
+            self._log("characteristicが不明のためコントロールできません")
+            return
+        await char.write(value)
+
+
+class ResponsiveDeviceManager(ControllableDeviceManager):
+    def __init__(
+        self,
+        name,
+        control_service_id,
+        control_char_id,
+        response_service_id,
+        response_char_id,
+    ):
+        super().__init__(name, control_service_id, control_char_id)
+        self.response_service_id = response_service_id
+        self.response_char_id = response_char_id
+
+    async def listen_response(
+        self,
+        callback=lambda data: print(
+            f"レスポンスを受け取りましたがコールバックが設定されていません\n\tdata:{data}"
+        ),
+    ):
+        char = await self.get_characteristic(
+            self.response_service_id, self.response_char_id
+        )
+        if char is None:
+            self._log("charactaristicが不明のためresponseを待てません")
+            return
+        data = await char.notified()
+        callback(data)
