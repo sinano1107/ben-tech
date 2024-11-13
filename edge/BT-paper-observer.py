615842d69c521b8ca5e4ab01b9dd07a65c041557
sinano1107 <tftf161107@icloud.com>
2024-11-13 19:14:00 +0900

    update 回転数の算出に分散を使用

--- edge/BT-paper-observer.py
+++ edge/BT-paper-observer.py
@@ -7,6 +7,30 @@ from machine import ADC, Pin
 from common import BenTechResponsiveDeviceServer
 
 
+class FixedList:
+    def __init__(self, size):
+        self.size = size
+        self.data = []
+
+    def add(self, value):
+        # リストの長さが指定されたサイズよりも大きければ、古い要素を削除
+        if len(self.data) >= self.size:
+            self.data.pop(0)
+        self.data.append(value)
+
+    def variance(self):
+        # 要素がない場合、分散はゼロとする
+        if len(self.data) == 0:
+            return 0
+
+        # 平均を計算
+        mean = sum(self.data) / len(self.data)
+
+        # 分散を計算
+        variance = sum((x - mean) ** 2 for x in self.data) / len(self.data)
+        return variance
+
+
 class Counter:
     def __init__(self):
         # これないとうまく動かない
@@ -17,19 +41,25 @@ class Counter:
 
         self.should_count = False
         self.current_flag = False
-        self.threshold = 0.9
+        self.threshold = 0.005
         self.interval = 100
         self.roll = 0
         self.count = 0
         self.coef = 3.3 / 65535
 
+        self.list = FixedList(5)
+
     async def _count_cycle(self):
         v = self.adc.read_u16() * self.coeff
-        print("V = {:.2f}, roll = {}".format(v, self.roll))
-        print("=" * int(v / 0.03))
-        await asyncio.sleep(0.01)
 
-        flag = v >= self.threshold and self.count >= self.interval
+        self.list.add(v)
+        variance = self.list.variance()
+
+        # print("=" * int(v / 0.03))
+        print("-" * int(variance / 0.001))
+        print("V = {:.2f}, roll = {}, variance = {:.2f}".format(v, self.roll, variance))
+
+        flag = variance >= self.threshold and self.count >= self.interval
 
         if flag != self.current_flag:
             if flag:
@@ -41,6 +71,7 @@ class Counter:
             self.current_flag = flag
 
         self.count += 1
+        await asyncio.sleep(0.01)
 
     def start(self):
         self.count = 0
@@ -115,6 +146,14 @@ class PaperObserver(BenTechResponsiveDeviceServer):
 async def main():
     observer = PaperObserver()
     await observer.run()
+
+    # counterだけテスト
+    """
+    counter = Counter()
+    counter.start()
+    task = asyncio.create_task(counter.run())
+    await task
+    """
 
 
 if __name__ == "__main__":
