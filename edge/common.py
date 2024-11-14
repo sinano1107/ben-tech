9a85f3c4ae3a1f6e6dd4f4beee1f957c0d42329e
sinano1107 <tftf161107@icloud.com>
2024-11-14 18:12:59 +0900

    fix 一度streamで送信されると、以降controlなども何も受け付けなくなるバグ

--- edge/common.py
+++ edge/common.py
@@ -95,6 +95,8 @@ class BenTechStreamableDeviceServer(BenTechResponsiveDeviceServer):
         self.stream_char = aioble.Characteristic(
             self.service, stream_char_id, write=True, notify=True, capture=True
         )
+        self.should_listen = False
+        self.listen_result = ""
 
     async def _send_stream(self, msg):
         byte_array = msg.encode("utf-8")
@@ -112,19 +114,54 @@ class BenTechStreamableDeviceServer(BenTechResponsiveDeviceServer):
         print("[_send_stream] 送信終了")
 
     async def _listen_stream(self):
-        _, data = await self.stream_char.written()
-        length = int.from_bytes(data)
-        print(
-            f"[_receive_socker_communication] これから{length}個のデータが送られてきます"
-        )
+        while self.connection.is_connected():
+            try:
+                _, data = await self.stream_char.written(timeout_ms=1000)
+                # フラグが立ってない時はブレーク
+                if not self.should_listen:
+                    print("Listenすべき時じゃないのでcontinueします")
+                    continue
 
-        joinned_data = bytes()
+                # まず送られてくるデータの長さを読む
+                length = int.from_bytes(data)
+                print(
+                    f"[_receive_socker_communication] これから{length}個のデータが送られてきます"
+                )
 
-        for _ in range(length):
-            _, data = await self.stream_char.written()
-            print(data)
-            joinned_data += data
+                # 連結済みのbytes
+                joinned_data = bytes()
 
-        msg = joinned_data.decode("utf-8")
-        print(f"[_receive_socker_communication] メッセージを受け取りました\n\t{msg}")
-        return msg
+                # 受け取り次第連結
+                for _ in range(length):
+                    _, data = await self.stream_char.written()
+                    joinned_data += data
+
+                msg = joinned_data.decode("utf-8")
+                print(
+                    f"[_receive_socker_communication] メッセージを受け取りました\n\t{msg}"
+                )
+                self.listen_result = msg
+                self.should_listen = False
+
+            except asyncio.TimeoutError:
+                pass
+        self.connection = None
+        print("接続が切断されたためStreamの受付を終了しました")
+
+    async def start_listen(self):
+        self.should_listen = True
+        while self.should_listen:
+            await asyncio.sleep(0.1)
+        return self.listen_result
+
+    async def run(self):
+        aioble.register_services(self.service)
+
+        while True:
+            await self._wait_to_connect()
+
+            listen_control_task = asyncio.create_task(self._listen_control())
+            listen_stream_task = asyncio.create_task(self._listen_stream())
+
+            await listen_control_task
+            await listen_stream_task
